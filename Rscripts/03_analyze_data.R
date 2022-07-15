49eea81e0f63729e6dae2de9f7f6818e1ebff1d8
schochastics <david@schochastics.net>
2022-07-15 09:24:11 +0200

    added overlap calculation

--- Rscripts/03_analyze_data.R
+++ Rscripts/03_analyze_data.R
@@ -19,7 +19,8 @@ news <- lapply(cutoffs,function(y) {sapply(fl,function(x){
   }
   dt <- dt[type!="" & duration>=y]
   dt[survey, on = .(panelist_id), leftright := leftright]
-  dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  # dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
   dt1 <- unique(dt[,.(domain,panelist_id,dem,rep)])
   # peeps <- unique(dt1[,.(panelist_id,dem,rep)])
   nd <- sum(dt1$dem,na.rm = TRUE)
@@ -41,7 +42,8 @@ pol <- lapply(cutoffs,function(y) {sapply(fl,function(x){
   }
   dt <- dt[type!="" & political=="political"& duration>=y]
   dt[survey, on = .(panelist_id), leftright := leftright]
-  dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  # dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
   dt1 <- unique(dt[,.(domain,panelist_id,dem,rep)])
   nd <- sum(dt1$dem,na.rm = TRUE)
   nr <- sum(dt1$rep,na.rm = TRUE)
@@ -75,7 +77,8 @@ news <- lapply(cutoffs,function(y) {sapply(fl,function(x){
   }
   dt <- dt[type!="" & duration>=y]
   dt[survey, on = .(panelist_id), leftright := leftright]
-  dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  # dt[leftright!=6,`:=`(dem=fifelse(leftright<=5,1,0),rep=fifelse(leftright>=7,1,0))]
+  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
   dt1 <- unique(dt[,.(domain,panelist_id,dem,rep)])
   # peeps <- unique(dt1[,.(panelist_id,dem,rep)])
   nd <- sum(dt1$dem,na.rm = TRUE)
@@ -87,79 +90,6 @@ news <- lapply(cutoffs,function(y) {sapply(fl,function(x){
   
 })
 })
-
-## individual news exposure (testing) ----
-news <- lapply(cutoffs,function(y) { lapply(fl,function(x){
-  dt <- data.table::fread(paste0("processed_data/tracking/",x))
-  if(fixN){
-    peeps <- unique(dt[political=="political"][,.(panelist_id)])[["panelist_id"]]
-    dt <- dt[panelist_id%in%peeps]
-  }
-  dt <- dt[type!=""& duration>=y]
-  dt[survey, on = .(panelist_id), leftright := leftright]
-  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
-  dt1 <- unique(dt[,.(domain,panelist_id,dem,rep,cen)])
-  # dt1 <- dt
-  dt1 <- dt1[!is.na(dem)]
-  # rep_scores <- dt1[,.(rep_frac=sum(rep)/sum(dem+rep+cen)),by=.(domain)]
-  rep_scores <- dt1[,.(rep_frac=sum(rep-dem)/sum(dem+rep)),by=.(domain)]
-  dt1[rep_scores,on = .(domain), rep_frac := rep_frac]
-  dt1 <- dt1[,.(avg_rep_exp=mean(rep_frac,na.rm=TRUE),ideo=fifelse(dem==1,-1,fifelse(cen==1,0,1))),.(panelist_id)]
-  dt1[,`:=`(cutoff=y,case=x)]
-})
-})
-
-pol <- lapply(cutoffs,function(y) { lapply(fl,function(x){
-  dt <- data.table::fread(paste0("processed_data/tracking/",x))
-  if(fixN){
-    peeps <- unique(dt[political=="political"][,.(panelist_id)])[["panelist_id"]]
-    dt <- dt[panelist_id%in%peeps]
-  }
-  dt <- dt[type!=""& political=="political" & duration>=y]
-  dt[survey, on = .(panelist_id), leftright := leftright]
-  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
-  dt1 <- unique(dt[,.(domain,panelist_id,dem,rep,cen)])
-  # dt1 <- dt
-  dt1 <- dt1[!is.na(dem)]
-  # rep_scores <- dt1[,.(rep_frac=sum(rep)/sum(dem+rep+cen)),by=.(domain)]
-  rep_scores <- dt1[,.(rep_frac=sum(rep-dem)/sum(dem+rep)),by=.(domain)]
-  dt1[rep_scores,on = .(domain), rep_frac := rep_frac]
-  dt1 <- dt1[,.(avg_rep_exp=mean(rep_frac,na.rm=TRUE),ideo=fifelse(dem==1,-1,fifelse(cen==1,0,1))),.(panelist_id)]
-  dt1[,`:=`(cutoff=y,case=x)]
-})
-})
-
-p1 <- map_dfr(news,identity,.id="id") |>
-  mutate(case=str_remove(case,"\\.csv")) |>
-  # mutate(cutoff=cutoffs[as.numeric(id)]) |>
-  # mutate(strip_lab = paste0("cutoff = ",cutoff)) |>
-  # mutate(strip_lab = fct_reorder(strip_lab,cutoff))|>
-  ggplot(aes(avg_rep_exp,group=ideo,fill=as.factor(ideo)))+
-  geom_density(alpha=0.5)+
-  scale_fill_manual(values=c("blue","grey66","red"),labels=c("left","center","right"),name="individual alignment")+
-  labs(x="favorability scores")+
-  facet_grid(case~cutoff,scales = "free_y")+
-  theme_minimal()+
-  theme(legend.position = "bottom")
-
-p2 <- map_dfr(pol,identity,.id="id") |>
-  mutate(case=str_remove(case,"\\.csv")) |>
-  # mutate(cutoff=cutoffs[as.numeric(id)]) |>
-  # mutate(strip_lab = paste0("cutoff = ",cutoff)) |>
-  # mutate(strip_lab = fct_reorder(strip_lab,cutoff))|>
-  ggplot(aes(avg_rep_exp,group=ideo,fill=as.factor(ideo)))+
-  geom_density(alpha=0.5)+
-  scale_fill_manual(values=c("blue","grey66","red"),labels=c("left","center","right"),name="individual alignment")+
-  labs(x="favorability scores",y="")+
-  facet_grid(case~cutoff,scales = "free_y")+
-  theme_minimal()+
-  theme(legend.position = "bottom")
-
-p1+p2+
-  plot_annotation(tag_levels = "a",tag_suffix = ")")+
-  plot_layout(ncol=2,guides = "collect") & theme(legend.position = 'bottom')
-
-ggsave("figures/old/explore/indiv_exposure5.png",width = 14*1.5,height=5*1.5,bg = "white")  
 
 
 pol <- lapply(cutoffs,function(y) {sapply(fl,function(x){
@@ -195,7 +125,123 @@ do.call("rbind",news) %>%
   mutate(case=str_extract(name,"^[a-z]{2}")) %>% 
   saveRDS("processed_data/dissimilarity_scores.RDS")
 
+
+## individual news exposure (testing) ----
+news <- lapply(cutoffs,function(y) { lapply(fl,function(x){
+  dt <- data.table::fread(paste0("processed_data/tracking/",x))
+  if(fixN){
+    peeps <- unique(dt[political=="political"][,.(panelist_id)])[["panelist_id"]]
+    dt <- dt[panelist_id%in%peeps]
+  }
+  dt <- dt[type!=""& duration>=y]
+  dt[survey, on = .(panelist_id), leftright := leftright]
+  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
+  dt1 <- unique(dt[,.(domain,panelist_id,dem,rep,cen)])
+  # dt1 <- dt
+  dt1 <- dt1[!is.na(dem)]
+  # rep_scores <- dt1[,.(rep_frac=sum(rep)/sum(dem+rep+cen)),by=.(domain)]
+  rep_scores <- dt1[,.(rep_frac=sum(rep-dem)/sum(dem+rep)),by=.(domain)]
+  dt1[rep_scores,on = .(domain), rep_frac := rep_frac]
+  dt2 <- dt1[,.(avg_rep_exp=mean(rep_frac,na.rm=TRUE)),by = .(panelist_id,dem,cen,rep)]
+  dt2[,`:=`(ideo=-dem+rep,cutoff=y,case=x)]
+  dt2[,.(panelist_id,avg_rep_exp,ideo,cutoff,case)]
+})
+})
+
+pol <- lapply(cutoffs,function(y) { lapply(fl,function(x){
+  dt <- data.table::fread(paste0("processed_data/tracking/",x))
+  if(fixN){
+    peeps <- unique(dt[political=="political"][,.(panelist_id)])[["panelist_id"]]
+    dt <- dt[panelist_id%in%peeps]
+  }
+  dt <- dt[type!=""& political=="political" & duration>=y]
+  dt[survey, on = .(panelist_id), leftright := leftright]
+  dt[,`:=`(dem=fifelse(leftright<=4,1,0),rep=fifelse(leftright>=8,1,0),cen=fifelse(leftright%in%c(5,6,7),1,0))]
+  dt1 <- unique(dt[,.(domain,panelist_id,dem,rep,cen)])
+  # dt1 <- dt
+  dt1 <- dt1[!is.na(dem)]
+  # rep_scores <- dt1[,.(rep_frac=sum(rep)/sum(dem+rep+cen)),by=.(domain)]
+  rep_scores <- dt1[,.(rep_frac=sum(rep-dem)/sum(dem+rep)),by=.(domain)]
+  dt1[rep_scores,on = .(domain), rep_frac := rep_frac]
+  dt2 <- dt1[,.(avg_rep_exp=mean(rep_frac,na.rm=TRUE)),by = .(panelist_id,dem,cen,rep)]
+  dt2[,`:=`(ideo=-dem+rep,cutoff=y,case=x)]
+  dt2[,.(panelist_id,avg_rep_exp,ideo,cutoff,case)]
+})
+})
+
+p1 <- map_dfr(news,identity,.id="id") |>
+  mutate(case=str_remove(case,"\\.csv")) |>
+  # mutate(cutoff=cutoffs[as.numeric(id)]) |>
+  # mutate(strip_lab = paste0("cutoff = ",cutoff)) |>
+  # mutate(strip_lab = fct_reorder(strip_lab,cutoff))|>
+  ggplot(aes(avg_rep_exp,group=ideo,fill=as.factor(ideo)))+
+  geom_density(alpha=0.5)+
+  scale_fill_manual(values=c("blue","grey66","red"),labels=c("left","center","right"),name="individual alignment")+
+  labs(x="favorability scores")+
+  facet_grid(case~cutoff,scales = "free_y")+
+  theme_minimal()+
+  theme(legend.position = "bottom")
+
+p2 <- map_dfr(pol,identity,.id="id") |>
+  mutate(case=str_remove(case,"\\.csv")) |>
+  # mutate(cutoff=cutoffs[as.numeric(id)]) |>
+  # mutate(strip_lab = paste0("cutoff = ",cutoff)) |>
+  # mutate(strip_lab = fct_reorder(strip_lab,cutoff))|>
+  ggplot(aes(avg_rep_exp,group=ideo,fill=as.factor(ideo)))+
+  geom_density(alpha=0.5)+
+  scale_fill_manual(values=c("blue","grey66","red"),labels=c("left","center","right"),name="individual alignment")+
+  labs(x="favorability scores",y="")+
+  facet_grid(case~cutoff,scales = "free_y")+
+  theme_minimal()+
+  theme(legend.position = "bottom")
+
+p1+p2+
+  plot_annotation(tag_levels = "a",tag_suffix = ")")+
+  plot_layout(ncol=2,guides = "collect") & theme(legend.position = 'bottom')
+
+ggsave("figures/old/explore/indiv_exposure6.png",width = 14*1.5,height=5*1.5,bg = "white")  
+
+dens_news <- map_dfr(news,identity,.id="id") |>
+  group_by(cutoff,case,ideo)|>
+  summarise(d=list(avg_rep_exp),.groups="drop")|>
+  mutate(case=str_remove_all(case,"\\.csv"))|>
+  pivot_wider(names_from = ideo,values_from = d)|>
+  rename(left=3,center=4,right=5)|>
+  rowwise()|>
+  mutate(coef=unname(overlap(list(X1=left,X2=right))$OV),
+         political="non-political")
+
+
+
+dens_pol <- map_dfr(pol,identity,.id="id") |>
+  group_by(cutoff,case,ideo)|>
+  summarise(d=list(avg_rep_exp),.groups="drop") |>
+  mutate(case=str_remove_all(case,"\\.csv"))|>
+  pivot_wider(names_from = ideo,values_from = d)|>
+  rename(left=3,center=4,right=5)|>
+  rowwise()|>
+  mutate(coef=unname(overlap(list(X1=left,X2=right))$OV),
+         political="political")
+
+bind_rows(dens_news,dens_pol)|>
+  ggplot(aes(x=cutoff,y=coef,col=case))+
+  geom_point()+
+  geom_line()+
+  scale_color_manual(values=pal,name="sample")+
+  theme_minimal()+
+  facet_wrap(political~.) +
+  theme_minimal()+
+  theme(
+    panel.grid.minor = element_blank(),
+    legend.position = "bottom",
+    strip.text = element_text(size=12))+
+  labs(x = "cutoff (in sec)",y = "overlap coefficient")+
+  guides(colour = guide_legend(override.aes = list(size=2),nrow = 1))
+
+ggsave("figures/old/explore/overlap_coef.png",width = 10,height=6,bg = "white")
+
 # plotting ----
+pal <- c("#9F248FFF", "#FFCE4EFF", "#017A4AFF", "#F9791EFF", "#244579FF", "#C6242DFF")
 ## segregation scores ----
 seg_scores <- readRDS("processed_data/segregation_scores.RDS")
 seg_scores_long <- seg_scores %>% 
@@ -213,7 +259,7 @@ ggplot(seg_scores_long) +
     legend.position = "bottom",
     strip.text = element_text(size=12))+
   scale_color_manual(values=pal,name="sample")+
-  scale_y_continuous(limits=c(0,0.3),breaks=c(0,0.1,0.2,0.3))+
+  scale_y_continuous(limits=c(0,0.35),breaks=c(0,0.1,0.2,0.3))+
   labs(x = "cutoff (in sec)",y = "segregation score")+
   guides(colour = guide_legend(override.aes = list(size=2),nrow = 1))
 
